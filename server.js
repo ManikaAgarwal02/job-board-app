99eb90269f5c57017e5bc282a0e5325364db223d
Manika Agarwal <manikaag29@gmail.com>
2026-05-26 18:47:13 +0530

    Setup MongoDB Atlas connection with Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express=require("express");
-const app=express();
-app.set("view engine","ejs");
-app.get("/",(req,res)=>{
+const express = require("express");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// EJS Setup
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
     res.render("home");
-})
-app.listen(3000,()=>{
-    console.log("app is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
